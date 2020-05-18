9fc2942a381a4bc26b582985cea1e16cd4fdea58
hzm6477 <1481646412@qq.com>
2020-05-18 18:40:18 +0800

    完成注册页面的校验功能

--- src/views/login/register.vue
+++ src/views/login/register.vue
@@ -3,23 +3,35 @@
     <el-dialog :visible.sync="dialogVisible" width="600px" center>
       <div slot="title" class="title">注册</div>
       <!-- 表单 -->
-      <el-form label-width="100px" :model="resgisterForm">
-        <el-form-item label="昵称">
+      <el-form label-width="100px" :model="resgisterForm" :rules="rules">
+        <el-form-item label="头像" prop="username">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="昵称" prop="username">
           <el-input v-model="resgisterForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱">
+        <el-form-item label="邮箱" prop="email">
           <el-input v-model="resgisterForm.email"></el-input>
         </el-form-item>
-        <el-form-item label="手机">
+        <el-form-item label="手机" prop="phone">
           <el-input v-model="resgisterForm.phone"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input show-password v-model="resgisterForm.password"></el-input>
         </el-form-item>
-        <el-form-item label="图形码">
+        <el-form-item label="图形码" prop="code">
           <el-row :gutter="20">
             <el-col :span="15">
-              <el-input></el-input>
+              <el-input v-model="resgisterForm.code"></el-input>
             </el-col>
             <el-col :span="8">
               <img
@@ -30,7 +42,7 @@
             </el-col>
           </el-row>
         </el-form-item>
-        <el-form-item label="验证码">
+        <el-form-item label="验证码" prop="rcode">
           <el-row>
             <el-col :span="17">
               <el-input v-model="resgisterForm.rcode"></el-input>
@@ -41,7 +53,6 @@
           </el-row>
         </el-form-item>
       </el-form>
-
       <span slot="footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -62,7 +73,75 @@ export default {
         email: "", // 邮箱
         avatar: "", // 头像地址
         password: "", // 密码
+        code: "", // 图形码
         rcode: "" // 验证码
+      },
+      rules: {
+        username: [
+          { required: true, message: "必须输入用户名", trigger: "blur" }
+        ],
+
+        email: [
+          {
+            required: true,
+            validator: (rule, value, callback) => {
+              if (!value) {
+                callback(new Error("邮箱不能为空"));
+                return;
+              }
+              // 邮箱的正则
+              const reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+              if (!reg.test(value)) {
+                return callback(new Error("邮箱不合法!"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ],
+        phone: [
+          {
+            required: true,
+            validator: (rule, value, callback) => {
+              if (!value) {
+                callback(new Error("手机号能为空"));
+                return;
+              }
+              // 邮箱的正则
+              const reg = /^1[3456789][0-9]{9}$/;
+              if (!reg.test(value)) {
+                return callback(new Error("手机号不合法!"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, message: "必须数六位", trigger: "blur" }
+        ],
+        code: [
+          { required: true, message: "图形码不能为空", trigger: "blur" },
+          { min: 4, max: 4, message: "图形码必须是4位", trigger: "blur" }
+        ],
+        rcode: [
+          // { required: true, message: "验证码不能为空", trigger: "blur" },
+          // { min: 4, max: 4, message: "验证码必须是4位", trigger: "blur" },
+          {
+            required: true,
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error("验证码不能为空"));
+              }
+              if (!Number.isInteger(value)) {
+                return callback(new Error("验证码必须是数字"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ]
       }
     };
   }
@@ -87,6 +166,32 @@ export default {
   .captcha {
     width: 100%;
     height: 40px;
+  }
+  .avatar-uploader {
+    text-align: center;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 }
 </style>
