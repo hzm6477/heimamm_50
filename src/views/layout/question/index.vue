999428143f27a96b72a3e397ea07a5b4da1b2b3d
hzm6477 <1481646412@qq.com>
2020-05-26 01:10:32 +0800

    完成了题库列表头部的渲染

---
+++ src/views/layout/question/index.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="question">
+    <!-- 搜索内容区域 -->
+    <el-card>
+      <el-form inline :model="searchForm" ref="form" label-width="50px">
+        <el-row>
+          <el-col :span="6">
+            <el-form-item class="selectWidth" label="学科" prop="subject">
+              <el-select v-model="searchForm.subject" placeholder="请选择学科">
+                <el-option
+                  v-for="item in subjectList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="阶段" prop="step">
+              <el-select v-model="searchForm.step" placeholder="请选择阶段">
+                <el-option
+                  v-for="(value, name) in stepObj"
+                  :key="name"
+                  :label="value"
+                  :value="name"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="企业" prop="enterprise">
+              <el-select
+                v-model="searchForm.enterprise"
+                placeholder="请选择企业"
+              >
+                <el-option
+                  v-for="item in enterpriseList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="题型" prop="type">
+              <el-select v-model="searchForm.type" placeholder="请选择题型">
+                <el-option
+                  v-for="(value, name) in typeObj"
+                  :key="name"
+                  :label="value"
+                  :value="name"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6">
+            <el-form-item label="难度" prop="difficulty">
+              <el-select
+                v-model="searchForm.difficulty"
+                placeholder="请选择难度"
+              >
+                <el-option
+                  v-for="(value, name) in difficultyObj"
+                  :key="name"
+                  :label="value"
+                  :value="name"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="作者" prop="username">
+              <el-input
+                style="width:208px;"
+                v-model="searchForm.username"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="状态" prop="status">
+              <el-select v-model="searchForm.status" placeholder="请选择状态">
+                <el-option label="启用" value="1"> </el-option>
+                <el-option label="禁用" value="0"> </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="日期" prop="create_date">
+              <el-date-picker
+                v-model="searchForm.create_date"
+                type="date"
+                placeholder="选择日期"
+              >
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="标题" prop="title">
+              <el-input
+                style="width:500px"
+                v-model="searchForm.title"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item>
+              <el-button type="primary">搜索</el-button>
+              <el-button>清除</el-button>
+              <el-button type="primary">+新增试题</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-card>
+    <!-- 列表区域 -->
+    <el-card style="margin-top:15px;"> </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  data() {
+    return {
+      subjectList: [], // 学科列表
+      enterpriseList: [], // 企业列表
+      stepObj: { 1: "初级", 2: "中级", 3: "高级" }, //阶段
+      typeObj: { 1: "单选", 2: "多选", 3: "简答" }, //题型
+      difficultyObj: { 1: "简单", 2: "一般", 3: "困难" }, //难度
+
+      searchForm: {
+        // 搜索的时候要传递给后台的
+        subject: "", // 学科
+        enterprise: "", // 企业
+        step: "", // 阶段
+        type: "", // 题型
+        difficulty: "", // 难度
+        username: "", // 作者
+        status: "", // 状态 0 禁用 1 启用
+        create_date: "", // 创建日期
+        title: "", // 标题
+      },
+    };
+  },
+  created() {
+    // 获取所有的学科
+    this.getSubjectListData();
+    // 获取所有的企业
+    this.getEnterpriseListData();
+  },
+  methods: {
+    async getSubjectListData() {
+      const res = await this.$axios.get("/subject/list");
+
+      if (res.data.code === 200) {
+        this.subjectList = res.data.data.items;
+      }
+    },
+    async getEnterpriseListData() {
+      const res = await this.$axios.get("/enterprise/list");
+
+      if (res.data.code === 200) {
+        this.enterpriseList = res.data.data.items;
+      }
+    },
+  },
+};
+</script>
